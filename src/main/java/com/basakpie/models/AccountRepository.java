2a3a38f63e4657d2798a1539833cb5fc10475bbd
raisyousaf <raisyousaf7@gmail.com>
2017-07-22 13:52:12 +0000

    Account View Added

---
+++ src/main/java/com/basakpie/models/AccountRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.basakpie.models;
+
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ *
+ * @author annonymous
+ */
+public interface AccountRepository extends JpaRepository<Account, Long>{
+   
+ 
+}
